24b5b4ce33e5c00fcfd9985b39690821a4c0f1f3
Makc <m_stefanenkov@mail.ru>
2023-02-25 12:22:07 +0100

    Решил задачу 54

--- Task54/Program.cs
+++ Task54/Program.cs
@@ -7,3 +7,54 @@
 // 7 4 2 1
 // 9 5 3 2
 // 8 4 4 2
+
+int[,] GenerateMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+void SortNumbers(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int n = 0; n < matrix.GetLength(1) - 1; n++)
+            {
+                if (matrix[i, n] < matrix[i, n + 1])
+                {
+                    int temp = matrix[i, n + 1];
+                    matrix[i, n + 1] = matrix[i, n];
+                    matrix[i, n] = temp;
+                }
+            }
+        }
+    }
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5}   |");
+            else Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine("   |");
+    }
+    Console.WriteLine();
+}
+
+int[,] matr = GenerateMatrix(4, 4, 0, 10);
+PrintMatrix(matr);
+SortNumbers(matr);
+PrintMatrix(matr);
